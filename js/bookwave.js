4ca338b53b331f045595064596ecc05534fa4194
David Goligorsky <d.goligorsky@gmail.com>
2018-12-08 08:23:12 -0500

    adding displacement textures and moving those textures across the base image and making it wrap back around in a loop

--- js/bookwave.js
+++ js/bookwave.js
@@ -20,26 +20,49 @@ sections.forEach(section => {
     // add the pixi application to the section tag
     section.appendChild(app.view)
 
-    // make a new image 
+    // initialize/make a new image 
     let image = null
+    let displacementImage = null
 
     // make a new loader
     const loader = new PIXI.loaders.Loader()
 
     // load in our image
     loader.add("image", originalImageSource)
+    loader.add("displacement", "displacement1.jpg")
     loader.load((loader, resources) => {
         // once the image has loaded, now do things
         image = new PIXI.Sprite(resources.image.texture)
+        displacementImage = new PIXI.Sprite(resources.displacement.texture)
 
-        image.x = 100
-        image.y = 100
+        image.x = 100 + 450
+        image.y = 100 + 300
         image.width = 900
         image.height = 600
         image.interactive = true
 
+        image.anchor.x = 0.5;
+        image.anchor.y = 0.5;
+
+        displacementImage.width = 300
+        displacementImage.height = 300
+        displacementImage.texture.baseTexture.wrapMode = PIXI.WRAP_MODES.REPEAT
+
+
+        image.filters = [
+            // new PIXI.filters.BlurFilter(3, 5),
+            // new PIXI.filters.NoiseFilter(.1),
+            new PIXI.filters.DisplacementFilter(displacementImage, 100)
+        ]
+
     // add the image to the app
     app.stage.addChild(image)
+    app.stage.addChild(displacementImage)
+
+    app.ticker.add(() => {
+        displacementImage.x = displacementImage.x + 0.01;
+    })
+
     })
 
 })
